ab06ec24685d8eac9b10b63da76d748e9973e056
TMC\mehrnoosh.rigi <mehrnoosh.rigi@tmc.nl>
2022-09-05 14:48:44 +0200

    Organize student controller API

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -1,13 +1,8 @@
 package com.example.springboot;
 
-import com.example.springboot.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -17,14 +12,5 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@GetMapping
-	public List<Student> Hello(){
-		return List.of(new Student(
-				1L,
-				"student 1",
-				"student.1@gmail.com",
-				LocalDate.of(2000, 01, 25),
-				22
-		));
-	}
+
 }
