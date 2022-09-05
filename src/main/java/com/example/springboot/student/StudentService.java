2bc500a0b4a68be009dca73cfc04b7777de4321c
TMC\mehrnoosh.rigi <mehrnoosh.rigi@tmc.nl>
2022-09-05 18:18:12 +0200

    create data access layer

--- src/main/java/com/example/springboot/student/StudentService.java
+++ src/main/java/com/example/springboot/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.springboot.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -7,13 +8,21 @@ import java.util.List;
 
 @Service
 public class StudentService {
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> GetStudent(){
-        return List.of(new Student(
-                1L,
-                "student 1",
-                "student.1@gmail.com",
-                LocalDate.of(2000, 01, 25),
-                22
-        ));
+        return studentRepository.findAll();
+//                List.of(new Student(
+//                1L,
+//                "student 1",
+//                "student.1@gmail.com",
+//                LocalDate.of(2000, 01, 25),
+//                22
+//        ));
     }
 }
