aef24bbefc8413faeb47a6430ea434db8f58c70e
TMC\mehrnoosh.rigi <mehrnoosh.rigi@tmc.nl>
2022-09-06 11:53:33 +0200

    Add new student

--- src/main/java/com/example/springboot/student/StudentService.java
+++ src/main/java/com/example/springboot/student/StudentService.java
@@ -3,8 +3,8 @@ package com.example.springboot.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -15,18 +15,16 @@ public class StudentService {
         this.studentRepository = studentRepository;
     }
 
-    public static void addNewStudent(Student student) {
-        System.out.println(student);
-    }
 
     public List<Student> GetStudent(){
         return studentRepository.findAll();
-//                List.of(new Student(
-//                1L,
-//                "student 1",
-//                "student.1@gmail.com",
-//                LocalDate.of(2000, 01, 25),
-//                22
-//        ));
+    }
+
+    public void addNewStudent(Student student) {
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+        if (studentOptional.isPresent()){
+            throw new IllegalArgumentException("email taken");
+        }
+        studentRepository.save(student);
     }
 }
